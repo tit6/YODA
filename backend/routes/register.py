158481af5951bd115af6f9b92c741901546137cc
unhumain <titouan.pelou@esna.bzh>
2025-11-28 10:20:03 +0100

    Refactor registration logic to include second password validation and improve password hashing

--- backend/routes/register.py
+++ backend/routes/register.py
@@ -14,19 +14,28 @@ def register():
     prenom = request.json.get("prenom")
     email = request.json.get("email")
     password = request.json.get("password")
+    second_password = request.json.get("second_password")
 
     print(f"Registering user: {name}, {email}, {password}")
     if not name or not email or not password:
         return jsonify({"status": "error"}), 400
 
+
     #bcrypt wait the bytes
     motdepasse_bytes = password.encode("utf-8")
+    second_password_bytes = second_password.encode("utf-8")
 
+    salt = gensalt()
     # generate salt and hash
-    hash_bytes = hashpw(motdepasse_bytes, gensalt())
+    hash_bytes = hashpw(motdepasse_bytes, salt)
+    second_hash_bytes = hashpw(second_password_bytes, salt)
 
-    # save in utf8
+    # save in utf8 and check password
     hash_str = hash_bytes.decode("utf-8")
+    second_hash_str = second_hash_bytes.decode("utf-8")
+
+    if hash_str != second_hash_str:
+        return jsonify({"status": "error"}), 400
 
     try:
         rowcount, user_id = execute_write(
